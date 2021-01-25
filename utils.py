8aad6e7d4de3f3fe9f4c73836d8876ac6ee337d1
rushiv0609 <rushiv0609@gmail.com>
2021-01-25 18:57:54 +0530

    Adding All .py files

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ def download_data():
     if not os.path.isdir(dir_name): # if directory not present then download and unzip
         if not os.path.exists(zip_file):
             url = 'https://drive.google.com/uc?id=1n-jwJulLoPraTe7KImctFjhsvufi_6yq'
-            gdown.download(url, output=zipfile, quiet=False)
+            gdown.download(url, output=zip_file, quiet=False)
         
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(".")
