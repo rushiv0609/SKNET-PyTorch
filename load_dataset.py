3576eabab7a2968685ce35f3db9b972d4f113362
rushiv0609 <rushiv0609@gmail.com>
2021-01-25 18:53:16 +0530

    Adding All .py files

--- load_dataset.py
+++ load_dataset.py
@@ -10,7 +10,7 @@ def download():
     if not os.path.isdir(dir_name): # if directory not present then download and unzip
         if not os.path.exists(zip_file):
             url = 'https://drive.google.com/uc?id=1n-jwJulLoPraTe7KImctFjhsvufi_6yq'
-            gdown.download(url, quiet=False)
+            gdown.download(url, output=zip_file, quiet=False)
         
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(".")
