c55cd8d9cdd7d7bb9f0e008f9d649f592b03bdfc
rushiv0609 <rushiv0609@gmail.com>
2021-02-07 00:26:45 +0530

    Added argparser to parse cmd arg. for no. of groups

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import torch
 from SKNET import SKNet
 import utils
 from train_test import train,test
+import argparse
 
+'''
+Define parser to get groups as cmd input
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("-G", default = 1, help="number of conv groups in model")
+args = parser.parse_args()
 
 '''
 Downloading & importing dataset
@@ -18,7 +25,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 num_classes = 200
-net = net = SKNet(200, [2,2,2,2], [1,2,2,2])
+net = SKNet(200, [2,2,2,2], [1,2,2,2], G = args.G)
 net.to(device)
 print("# of Parameters : ",sum([p.numel() for p in net.parameters()]))
 
