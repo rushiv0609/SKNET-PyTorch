d6546d518be7029232e74baa0fcbeaa316992b18
rushiv0609 <rushiv0609@gmail.com>
2021-04-25 13:29:18 +0530

    checking cyclic lr

--- train_test.py
+++ train_test.py
@@ -36,10 +36,7 @@ def train(net, device, train_loader, val_loader, cyclic = False, epochs = 30, lr
         scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer,
                                                       base_lr = 1e-5,
                                                       max_lr = 4e-4,
-                                                      step_size_up = num_batches,
-                                                      step_size_down = num_batches,
-                                                      mode = 'exp_range',
-                                                      gamma = 0.95,
+                                                      step_size_up = 2*num_batches,
                                                       cycle_momentum = False)
     else :
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min',
